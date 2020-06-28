e99b4c081b5673756caf0449c7f5eb6175be60f9
Slava V. Lysakov <slava@lysakov.ru>
2020-06-28 10:47:08 +0300

    Домашнее задание к уроку 3

--- vk/MainController.swift
+++ vk/MainController.swift
@@ -4,24 +4,11 @@ class MainController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let newsVC = UIViewController()
-        let bColor: UIColor = traitCollection.userInterfaceStyle == .dark ? .black : .white
-        newsVC.view.backgroundColor = bColor
-        newsVC.title = "News"
-        newsVC.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "newsfeed_28"), selectedImage: UIImage(named: "newsfeed_28"))
-        let friendsVC = FriendsViewController()
-        friendsVC.title = "Друзья"
-        friendsVC.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "users_28"), selectedImage: UIImage(named: "users_28"))
         let profileVC = UserProfileViewController()
-        profileVC.view.backgroundColor = bColor
+        profileVC.view.backgroundColor = traitCollection.userInterfaceStyle == .dark ? .black : .white
         profileVC.title = "Profile"
         profileVC.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "user_circle_28"), selectedImage: UIImage(named: "user_circle_28"))
-        
-//        let groupsVC = GroupViewController()
-//        groupsVC.view.backgroundColor = bColor
-//        groupsVC.title = "Сообщества"
-//        groupsVC.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "users3_28"), selectedImage: UIImage(named: "users3_28"))
-        
+
         self.viewControllers = [UINavigationController(rootViewController: profileVC)]
         
         print("UserId = \(Session.instance.userId ?? "nil")")
