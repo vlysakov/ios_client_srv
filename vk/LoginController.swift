502d54043ece74a02e81af82be691a16ced8f5d6
Slava V. Lysakov <slava@lysakov.ru>
2020-07-04 10:16:07 +0300

    Испрпавление мелких ошибок

--- vk/LoginController.swift
+++ vk/LoginController.swift
@@ -29,6 +29,7 @@ class LoginController: UIViewController, WKNavigationDelegate {
         urlComponents.host = "oauth.vk.com"
         urlComponents.path = "/authorize"
         urlComponents.queryItems = [
+//            URLQueryItem(name: "client_id", value: "7506747"),
             URLQueryItem(name: "client_id", value: "7522714"),
             URLQueryItem(name: "display", value: "mobile"),
             URLQueryItem(name: "redirect_uri", value: "https://oauth.vk.com/blank.html"),
@@ -62,6 +63,7 @@ class LoginController: UIViewController, WKNavigationDelegate {
                 dict[key] = value
                 return dict
         }
+        
         guard let userId = params["user_id"], let accessToken = params["access_token"], let expiresIn = params["expires_in"] else {
             decisionHandler(.cancel)
             return
